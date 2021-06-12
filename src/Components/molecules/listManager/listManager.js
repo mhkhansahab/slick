537f3b9c2aa1f64c856d1219622114a95547c6ac
Muhammad Hamza Khan <hamza.khansahab@gmail.com>
2021-06-12 18:51:05 +0500

    nested routes updated

--- src/Components/molecules/listManager/listManager.js
+++ src/Components/molecules/listManager/listManager.js
@@ -9,13 +9,13 @@ function listManager(props) {
     <div className="banner-container">
       <div className="management-nav flex center-2">
         <div className="flex center-2">
-        <NavLink to="/management/banner" style={{textDecoration:"none"}}>
+        <NavLink to="/slick//management/banner" style={{textDecoration:"none"}}>
           <div className="management-links">
             <div className="management-links-text" style={props.isBanner ? {color:"#6F8AE8"}: null}>Manage Banner</div>
             <div className="management-links-border" style={props.isBanner ? {display:"flex"}: null}></div>
           </div>
         </NavLink>
-        <NavLink to="/management/product" style={{textDecoration:"none"}}>
+        <NavLink to="/slick/management/product" style={{textDecoration:"none"}}>
           <div className="management-links product">
             <div className="management-links-text" style={props.isBanner ? null: {color:"#6F8AE8"}}>Manage Products</div>
             <div className="management-links-border" style={props.isBanner ? null: {display:"flex"}}></div>
